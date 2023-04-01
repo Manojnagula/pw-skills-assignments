027019288ec4b0e78a0190f0a414463910cada9c
Manojnagula <122856265+Manojnagula@users.noreply.github.com>
2023-04-01 17:29:20 +0530

    Add files via upload

---
+++ error handling and prototypes/7. Password Checker./script-3.js
@@ -0,0 +1,33 @@
+class User {
+  constructor(username, password) {
+    this.username = username;
+    this._password = password;
+  }
+
+  get password() {
+    return this._password.replace(/./g, "*");
+  }
+
+  set password(newPassword) {
+    if (
+      newPassword.length < 8 ||
+      !/\d/.test(newPassword) ||
+      !/[A-Z]/.test(newPassword)
+    ) {
+      console.error(
+        "Invalid password. Password must be at least 8 characters long and contain at least one number and one uppercase letter."
+      );
+      return;
+    }
+    this._password = newPassword;
+  }
+}
+const user = new User('john', 'password');
+console.log(user.password); 
+
+user.password = 'newpassword';
+console.log(user.password); 
+
+user.password = 'weak';
+
+console.log(user.password); 
