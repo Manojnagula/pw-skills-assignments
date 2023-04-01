040006dc1813d8ef8f1f953c5f73e1864747144f
Manojnagula <122856265+Manojnagula@users.noreply.github.com>
2023-04-01 17:41:55 +0530

    Update script-3.js

--- error handling and prototypes/7. Password Checker./script-3.js
+++ error handling and prototypes/7. Password Checker./script-3.js
@@ -5,29 +5,23 @@ class User {
   }
 
   get password() {
-    return this._password.replace(/./g, "*");
+    return this._password.replace(/./g, '*');
   }
 
   set password(newPassword) {
-    if (
-      newPassword.length < 8 ||
-      !/\d/.test(newPassword) ||
-      !/[A-Z]/.test(newPassword)
-    ) {
-      console.error(
-        "Invalid password. Password must be at least 8 characters long and contain at least one number and one uppercase letter."
-      );
+    if (newPassword.length < 8 || !/\d/.test(newPassword) || !/[A-Z]/.test(newPassword)) {
+      console.error('Invalid password. Password must be at least 8 characters long and contain at least one number and one uppercase letter.');
       return;
     }
     this._password = newPassword;
   }
 }
 const user = new User('john', 'password');
-console.log(user.password); 
+console.log(user.password); // Output: ********
 
 user.password = 'newpassword';
-console.log(user.password); 
+console.log(user.password); // Output: ********
 
 user.password = 'weak';
-
-console.log(user.password); 
+// Output: Invalid password. Password must be at least 8 characters long and contain at least one number and one uppercase letter.
+console.log(user.password); // Output: ********
