4b20fd9e5257fcb67c216eec84a41c1b535c1416
Manojnagula <122856265+Manojnagula@users.noreply.github.com>
2023-04-07 14:49:13 +0530

    Add files via upload

---
+++ 3rd april assignments/4.fourth demonstration of hoisting./script.js
@@ -0,0 +1,13 @@
+{
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+    let a = 10;
+    var b = 20;
+    const c = 30;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
