6c308a3aa429ade23055186a97628a427b07cb6d
Manojnagula <122856265+Manojnagula@users.noreply.github.com>
2023-07-06 16:26:20 +0530

    index.js

--- assignment01/08/index.js
+++ assignment01/08/index.js
@@ -6,9 +6,4 @@ const subscribeMessage = (channelName)=>{
     console.log(`you have subscribed to ${channelName}`);
 }
 eventemitter.addListener("subscribe",subscribeMessage);
-console.log(" calling before removing listener");
-eventemitter.emit("subscribe","college Wallah!");
-// removing listener
-eventemitter.removeListener("subscribe",subscribeMessage);
-console.log("calling after removal");
 eventemitter.emit("subscribe","college Wallah!");
