5639896e647bdcd5d90849898901e3affc59e54a
Manojnagula <122856265+Manojnagula@users.noreply.github.com>
2023-07-06 16:22:19 +0530

    Add files via upload

--- assignment01/08/index.js
+++ assignment01/08/index.js
@@ -6,4 +6,9 @@ const subscribeMessage = (channelName)=>{
     console.log(`you have subscribed to ${channelName}`);
 }
 eventemitter.addListener("subscribe",subscribeMessage);
+console.log(" calling before removing listener");
+eventemitter.emit("subscribe","college Wallah!");
+// removing listener
+eventemitter.removeListener("subscribe",subscribeMessage);
+console.log("calling after removal");
 eventemitter.emit("subscribe","college Wallah!");
