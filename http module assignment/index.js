a0b31b6576c040e26f814487a15a5826ffe1e41f
Manojnagula <122856265+Manojnagula@users.noreply.github.com>
2023-07-09 18:16:15 +0530

    Add files via upload

--- http module assignment/index.js
+++ http module assignment/index.js
@@ -1,64 +1,21 @@
-const http = require('http');
+const express = require('express')
 
-const server = http.createServer((req,res)=>{
-    res.setHeader('Content-Type','application/JSON')
+const app = express()
 
-    if(req.url ==='/')
-    {
-        res.statusCode=200;
-        res.end('welcome to men and women dummy store')
+let counter = 0;
 
-    }
-    else if(req.url ==='/men')
-    {   
-        const men = getProducts('men',10);
-        res.statusCode=200;
-        res.end(JSON.stringify(men))
+app.get('/',(req,res)=>{
+    res.json({counter})
+})
+app.post('/increment',(req,res)=>{
+    counter++;
+    res.json({counter})
+})
+app.get('/decrement',(req,res)=>{
+    counter--;
+    res.json({counter})
+})
 
-    }
-    else if(req.url ==='/women')
-    {   
-        const women = getProducts('women',10);
-        res.statusCode=200;
-        res.end(JSON.stringify(women))
-
-    }
-    else
-    {   
-    
-        res.statusCode=401;
-        res.end('page not found');
-
-    }
-
-});
-
-// creating products
-
-
-function getProducts(category, count)
-{
-    const products = [];
-
-for(let i = 1; i <= count;i++)
-{
-    const product = {
-        id : i,
-        name: `Product ${i}`,
-        brand: 'ABC',
-        price: 19.99,
-        color: 'Blue',
-        size: 'M',
-        category: category,
-    };
-    products.push(product)
-}
-return products;
-}
-
-// starting the server
-
-const PORT = 3000;
-server.listen(PORT,()=>{
-    console.log(`server started at the port ${PORT}`);
+app.listen(3001,()=>{
+    console.log('server started at port 3001');
 })
